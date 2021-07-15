5ce28991b373f6c2f6eeeaf9e7ef2327bce1cb35
martimmcdc <martimdams@gmail.com>
2021-07-15 16:18:31 +0100

    Update hub_simulation.py

--- hub_simulation.py
+++ hub_simulation.py
@@ -20,6 +20,17 @@ def gaussian2D(points,mx,my,ax,ay,N,C):
 	z = N * np.exp( - ax*(x-mx)**2 - ay*(y-my)**2 ) + C
 	return z
 
+def f(x,*args):
+	return(args[::2])
+
+def gaussian2Dmult(points,*args):
+	z = 0
+	for i in range(len(args)//6):
+		mx,my,ax,ay,N,C = args[6*i:6*(i+1)]
+		z += gaussian2D(points,mx,my,ax,ay,N,C)
+	return z
+
+
 def simulate(N):
 	""" Simulate noisy data to fit
 	"""
@@ -27,12 +38,12 @@ def simulate(N):
 	y = np.linspace(-1,1,N)
 	grid = np.meshgrid(x,y)
 	noise = np.random.normal(0,0.01,size=(N,N))
-	image = gaussian2D(grid,0.5,0.5,0.5,0.5,1,0)
-	return grid,image+noise
+	image = gaussian2D(grid,0,0,0.1,0.5,1,0)+gaussian2D(grid,0.5,0.5,0.1,0.1,0.7,0)
+	return grid,image#+noise
 
 def fit(grid,data,sat,peaks=1):
 	"""
-	Function takes array image and boolean array of same shape,
+	Function takes array image, its grid and boolean array of same shape,
 	which is True where pixels are saturated and False elsewhere.
 	Returns the image with saturated pixels corrected.
 	"""
@@ -67,28 +78,42 @@ def fit(grid,data,sat,peaks=1):
 
 if __name__ == '__main__':
 
-	grid,data = simulate(100)
-	sat = np.zeros(data.shape,bool)
-	for i in range(40,61):
-		for j in range(40,61):
-			a = np.random.randint(0,2,size=1)
-			if a==0:
-				b = np.random.randint(0,2,size=1)
-				sat[i,j] = bool(b)
-			else:
-				sat[i,j] = True
+	# grid,data = simulate(100)
+	# sat = np.zeros(data.shape,bool)
+	# for i in range(40,61):
+	# 	for j in range(40,61):
+	# 		a = np.random.randint(0,2,size=1)
+	# 		if a==0:
+	# 			b = np.random.randint(0,2,size=1)
+	# 			sat[i,j] = bool(b)
+	# 		else:
+	# 			sat[i,j] = True
 
-	plt.imshow(data)
-	plt.show()
-	plt.imshow(sat)
-	plt.show()
+	# plt.imshow(data)
+	# plt.show()
+	# plt.imshow(sat)
+	# plt.show()
 
-	data[sat] = 0
-	plt.imshow(data)
-	plt.show()
-	fit_data = fit(grid,data,sat)
+	# data2 = data.copy()
+	# data2[sat] = 0
+	# plt.imshow(data2)
+	# plt.show()
+	# fit_data = fit(grid,data2,sat)
 
-	plt.imshow(fit_data)
+	# plt.imshow(fit_data-data)
+	# plt.colorbar()
+	# plt.show()
+
+	x = np.linspace(-1,1,101)
+	y = x.copy()
+	grid = np.meshgrid(x,y)
+
+	mux1,muy1,ax1,ay1,N1,C1 = -0.3,-0.3,10,10,1,0 
+	mux2,muy2,ax2,ay2,N2,C2 = +0.3,-0.3,10,10,1,0 
+	mux3,muy3,ax3,ay3,N3,C3 = 0,0.6,10,10,1,0 
+
+	z = gaussian2Dmult(grid,mux1,muy1,ax1,ay1,N1,C1,mux2,muy2,ax2,ay2,N2,C2,mux3,muy3,ax3,ay3,N3,C3)
+	plt.imshow(z,origin='lower')
 	plt.show()
 
 
