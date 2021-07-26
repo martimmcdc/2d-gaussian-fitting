3528b4e5b4802671eae95134499f19bfbcc3583e
martimmcdc <martimdams@gmail.com>
2021-07-26 17:05:40 +0100

    Changes
    Corrected scale from degrees to pixels

--- hub_simulation.py
+++ hub_simulation.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-def gaussian2D(points,mx,my,ax,ay,N,C):
+def gaussian2D(points,mx,my,N):
 	"""
 	Gaussian function in 2D:
 		- points = (x,y) is the grid array at which the function is being evaluated
@@ -16,32 +16,33 @@ def gaussian2D(points,mx,my,ax,ay,N,C):
 		- N is an arbitrary normalization constant
 		- C is a baseline constant
 	"""
+	FWHM = 3*18.2/18 # pixels
+	sigma = FWHM/(2*np.sqrt(2*np.log(2)))
+	alpha = 1/(2*sigma**2)
 	x,y = points
-	z = N * np.exp( - ax*(x-mx)**2 - ay*(y-my)**2 ) + C
+	z = N * np.exp( - alpha*((x-mx)**2 + (y-my)**2) )
 	return z
 
-def f(x,*args):
-	return(args[::2])
-
 def gaussian2Dmult(points,*args):
+	""" Sum multiple 2D gaussian functions. """
 	z = 0
-	for i in range(len(args)//6):
-		mx,my,ax,ay,N,C = args[6*i:6*(i+1)]
-		z += gaussian2D(points,mx,my,ax,ay,N,C)
+	for i in range(len(args)//3):
+		mx,my,N = args[3*i:3*(i+1)]
+		z += gaussian2D(points,mx,my,N)
 	return z
 
 
 def simulate(N):
 	""" Simulate noisy data to fit
 	"""
-	x = np.linspace(-1,1,N)
-	y = np.linspace(-1,1,N)
+	x = np.linspace(-10,10,N)
+	y = x.copy()
 	grid = np.meshgrid(x,y)
-	noise = np.random.normal(0,0.01,size=(N,N))
-	image = gaussian2D(grid,-0.1,-0.2,10,5,1,0)+gaussian2D(grid,0.3,0.2,10,6,1,0)
+	noise = np.random.normal(0,1,size=(N,N))
+	image = gaussian2D(grid,-2,-1,1)+gaussian2D(grid,1,2,1.5)+gaussian2D(grid,1,-2,1)
 	return grid,image#+noise
 
-def fit(grid,data,sat,peaks=1,mu=None,FWHM=None):
+def fit(grid,data,sat,peaks=1):
 	"""
 	Function takes array image, its grid and boolean array of same shape,
 	which is True where pixels are saturated and False elsewhere.
@@ -51,23 +52,19 @@ def fit(grid,data,sat,peaks=1,mu=None,FWHM=None):
 	Nx,Ny = data.shape # number of points in x and y axes
 	X,Y = grid # index grid
 
-	if mu==None:
-		mu_x = np.floor(X[sat].mean())
-		mu_y = np.floor(Y[sat].mean())
-	else:
-		mu_x,mu_y = mu
+	mu_x = np.floor(X[sat].mean())
+	mu_y = np.floor(Y[sat].mean())
 
-	if FWHM==None:
-		sigma_x = X[sat].max() - X[sat].min()
-		sigma_y = Y[sat].max() - Y[sat].min()
-	else:
-		sigma_x,sigma_y = FWHM/np.sqrt(8*np.log(2))
-	N,C = data.max(),data.min()
-	peak_params = np.array([mu_x,mu_y,1/sigma_x**2,1/sigma_y**2,N,C])
+	FWHM = 3*18.2/18
+
+	N = data[np.isnan(data)==False].max()
+
+	peak_params = np.array([mu_x,mu_y,N])
 	guess_params = peak_params.copy()
 	for i in range(1,peaks):
-		var = np.random.normal(size=6)
-		guess_params = np.concatenate((guess_params,peak_params))
+		var = FWHM*np.random.normal(size=3)
+		guess_params = np.concatenate((guess_params,peak_params+var))
+
 
 	fit_x = np.empty([2,Ndata],float)
 	fit_data = np.empty(Ndata,float)
@@ -81,17 +78,16 @@ def fit(grid,data,sat,peaks=1,mu=None,FWHM=None):
 				fit_data[k] = data[i,j]
 			k += 1
 
-	params,cov = curve_fit(gaussian2Dmult,fit_x,fit_data,guess_params)
-	image = np.empty(data.shape)
-	image[sat] = gaussian2Dmult((X[sat],Y[sat]),*params)
+	params,cov = curve_fit(gaussian2Dmult,fit_x,fit_data,guess_params,maxfev=4000)
+	image = gaussian2Dmult((X,Y),*params)
 	image[sat==False] = data[sat==False]
 	return params,image
 
 if __name__ == '__main__':
 
-	grid,data = simulate(100)
+	grid,data = simulate(50)
 	x,y = grid[0][0,:],grid[1][:,0]
-	sat = data>0.95*data.max()
+	sat = data>0.5*data.max()
 	ticks = np.arange(0,100)
 	labels = np.linspace(-1,1,100)
 	plt.imshow(data)
@@ -103,11 +99,10 @@ if __name__ == '__main__':
 	data2[sat] = 0
 	plt.imshow(data2)
 	plt.show()
-	params,fit_data = fit(grid,data,sat,peaks=2)
+
+	params,fit_data = fit(grid,data,sat,peaks=3)
 
 	plt.imshow(fit_data)
-	plt.plot(50+50*params[0],50+50*params[1],'o')
-	plt.plot(50+50*params[6],50+50*params[7],'o')
 	plt.colorbar()
 	plt.show()
 
@@ -115,7 +110,6 @@ if __name__ == '__main__':
 	plt.colorbar()
 	plt.show()
 
-
-
-
-
+	print(params[:3])
+	print(params[3:6])
+	print(params[6:])
