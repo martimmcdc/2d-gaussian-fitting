769619715a2309840e4f74b73236599e98048112
martimmcdc <martimdams@gmail.com>
2021-10-07 18:15:41 +0100

    Updates

--- hub_tools.py
+++ hub_tools.py
@@ -47,7 +47,7 @@ def simulate(N):
 	image += gaussian(grid,1,-2,1,0,18.2,18.2)
 	return grid,image
 
-def fit(grid,data,sat,mu=[],theta=[],FWHM=[],peaks=1,helper_peaks=False):
+def fitter(grid,data,sat,mu=[],theta=[],FWHM=[],peaks=1,helper_peaks=False,var_pos=0.1,var_theta=0.1,var_FWHM=0.01):
 	"""
 	Function takes array image, its grid and boolean array of same shape,
 	which is True where pixels are saturated and False elsewhere.
@@ -65,30 +65,42 @@ def fit(grid,data,sat,mu=[],theta=[],FWHM=[],peaks=1,helper_peaks=False):
 		mu = np.array(peaks*[[mu_x,mu_y]])
 	N = data[np.isnan(data)==False].max()
 	if len(theta)==0:
-		theta = np.array(peaks*[0])
+		theta = np.array(peaks*[np.pi])
 	if len(FWHM)==0:
-		FWHM = np.array(peaks*[[18.2,18.2]])
+		FWHM = np.array(peaks*[[18.2,18.2]])/3600
 
+	lower_bounds,upper_bounds = [],[]
 	guess_params = np.empty(6*peaks)
 	for i in range(peaks):
 		mu_x,mu_y = mu[i,:]
 		FWHMx,FWHMy = FWHM[i,:]
-		guess_params[i*6:(i+1)*6] = [mu_x,mu_y,N,theta[i],FWHMx,FWHMy]
+		guess_params[i*6:(i+1)*6] = [mu_x,mu_y,N*1.1,theta[i],FWHMx,FWHMy]
+		
+		lower_bounds += [
+			X[sat].min() - var_pos,
+			Y[sat].min() - var_pos,
+			N,
+			theta[i] - var_theta,
+			FWHMx * (1 - var_FWHM),
+			FWHMy * (1 - var_FWHM)]
 
-	lower_bounds = peaks*[X[sat].min()-1,Y[sat].min()-1,     N,-np.pi,FWHMx*(1-1e-2),FWHMy*(1-1e-2)]
-	upper_bounds = peaks*[X[sat].max()+1,Y[sat].max()+1,np.inf, np.pi,FWHMx*(1+1e-2),FWHMy*(1+1e-2)]
+		upper_bounds += [
+			X[sat].max() + var_pos,
+			Y[sat].max() + var_pos,
+			np.inf,
+			theta[i] + var_theta,
+			FWHMx * (1 + var_FWHM),
+			FWHMy * (1 + var_FWHM)]
 
 	# add helper_peaks to the mix
 	if helper_peaks:
 		FWHMx,FWHMy = FWHM.mean(axis=0)
 
 		data0 = data[1:-1,1:-1]
-
 		prex = np.zeros(data.shape)
 		posx = prex.copy()
 		prey = prex.copy()
 		posy = prex.copy()
-
 		prex[1:-1,1:-1] = data0 - data[1:-1,:-2]
 		posx[1:-1,1:-1] = data0 - data[1:-1,2:]
 		prey[1:-1,1:-1] = data0 - data[:-2,1:-1]
@@ -102,8 +114,22 @@ def fit(grid,data,sat,mu=[],theta=[],FWHM=[],peaks=1,helper_peaks=False):
 		for i in range(m):
 			mu_x,mu_y,N = maxima[:,i]
 			helper_params[i*6:(i+1)*6] = [mu_x,mu_y,N,0,FWHMx,FWHMy]
-			lower_bounds += [mu_x*(1-1e-1),mu_y*(1-1e-1),N*(1-1e-1),-np.pi,FWHMx*(1-1e-2),FWHMy*(1-1e-2)]
-			upper_bounds += [mu_x*(1+1e-1),mu_y*(1+1e-1),N*(1+1e-1), np.pi,FWHMx*(1+1e-2),FWHMy*(1+1e-2)]
+
+			lower_bounds += [
+				mu_x - var_pos,
+				mu_y - var_pos,
+				N * 0.9,
+				-np.pi,
+				FWHMx * (1 - var_FWHM),
+				FWHMy * (1 - var_FWHM)]
+
+			upper_bounds += [
+				mu_x + var_pos,
+				mu_y + var_pos,
+				N * 1.1,
+				np.pi,
+				FWHMx * (1 + var_FWHM),
+				FWHMy * (1 + var_FWHM)]
 		guess_params = np.concatenate((guess_params,helper_params))
 
 	fit_x = np.array([X[sat==False],Y[sat==False]])
@@ -137,10 +163,8 @@ def display_fits(file,lims=[],return_vals=False):
 	data = hdu.data
 
 	# Get axes right
-	x = header['crval1'] + header['cdelt1']*np.arange(0,header['naxis1'],1)
-	y = header['crval2'] + header['cdelt2']*np.arange(0,header['naxis2'],1)
-	y = y[::-1] # reverse y-axis to have 0 index at top of grid
-
+	x = header['crval1'] + header['cdelt1'] * (np.arange(0,header['naxis1'],1) - header['crpix1'])
+	y = header['crval2'] + header['cdelt2'] * (np.arange(0,header['naxis2'],1) - header['crpix2'])
 	# If window limits are given
 	if len(lims)==0:
 		xl,xr,yb,yt = x.max(),x.min(),y.min(),y.max()
@@ -153,7 +177,7 @@ def display_fits(file,lims=[],return_vals=False):
 	data_sub = data[(y>=yb)&(y<=yt),:][:,(x<=xl)&(x>=xr)]
 
 	plt.figure(figsize=(8,8))
-	plt.imshow(np.log10(data_sub),extent=(xl,xr,yb,yt))
+	plt.imshow(np.log10(data_sub),origin='lower',extent=(xl,xr,yb,yt))
 	plt.show()
 
 	if return_vals:
